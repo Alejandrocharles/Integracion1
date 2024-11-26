9ce6a7e0a943308a38234a3db4fd7874d01ebf79
ALEJANDRO CHARLES GONZALEZ <110836700+Alejandrocharles@users.noreply.github.com>
2024-11-25 23:27:28 -0600

    Update functions.h

--- src/functions.h
+++ src/functions.h
@@ -1,13 +1,21 @@
+// functions.h
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
 #include <string>
 #include <utility>
 
-using namespace std;
+// Declaración de la función para leer un archivo y devolver su contenido como una cadena de texto
+std::string readFile(const std::string& filename);
 
-// Declaraciones de las funciones
-pair<pair<int, int>, string> findLongestPalindrome(const string &s);
-pair<int, int> findLongestCommonSubstring(const string &s1, const string &s2);
+// Declaración de la función para verificar si un código malicioso está contenido en una transmisión
+bool contains(const std::string& haystack, const std::string& needle, size_t& position);
+
+// Declaración de la función para encontrar el palíndromo más largo en una cadena
+std::pair<std::pair<int, int>, std::string> findLongestPalindrome(const std::string& s);
+
+// Declaración de la función para encontrar la subcadena más larga común entre dos cadenas
+std::pair<int, int> findLongestCommonSubstring(const std::string& s1, const std::string& s2);
 
 #endif // FUNCTIONS_H
